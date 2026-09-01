f0af833ef0bf3de5d1918c13b593d01069ac3888
kokchun <kokchungiang@gmail.com>
2026-09-01 14:33:18 +0200

    consumer nopt working yet

---
+++ src_pipeline/consumer.py
@@ -0,0 +1,17 @@
+import paho.mqtt.client as mqtt
+import json 
+
+def on_message(client, userdata, message):
+    # needs to decode because original msg is encoded after publishing
+    payload = message.payload.decode()
+
+    print(payload)
+
+
+
+if __name__ == "__main__":
+    client = mqtt.Client()
+    client.connect("mosquitto", 1883)
+    client.subscribe("home/pico/dht11")
+    client.on_message = on_message
+    client.loop_forever()
