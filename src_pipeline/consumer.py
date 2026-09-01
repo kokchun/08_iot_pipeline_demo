7bf4fe935b207d106bb84ce4fa880ad2bf42c222
kokchun <kokchungiang@gmail.com>
2026-09-01 14:33:45 +0200

    consumer nopt working yet

--- src_pipeline/consumer.py
+++ src_pipeline/consumer.py
@@ -11,7 +11,7 @@ def on_message(client, userdata, message):
 
 if __name__ == "__main__":
     client = mqtt.Client()
-    client.connect("mosquitto", 1883)
+    client.connect("localhost", 1883)
     client.subscribe("home/pico/dht11")
     client.on_message = on_message
     client.loop_forever()
