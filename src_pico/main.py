c068f4c5b087c9a24f5efbc4cf05fc7a1737b9c4
kokchun <kokchungiang@gmail.com>
2026-09-01 13:33:27 +0200

    connect MQTT template

--- src_pico/main.py
+++ src_pico/main.py
@@ -2,6 +2,7 @@ import time
 from wifi import connect_wifi
 from machine import Pin
 from dht import DHT11
+from umqtt.simple import MQTTClient
 
 time.sleep(0.1)
 
@@ -10,6 +11,14 @@ dht_sensor = DHT11(Pin(16))
 
 if connect_wifi():
     led.value(1)
+
+def connect_mqtt():
+    client = MQTTClient(client_id="pico", server="<IP_ADDRESS>", port=1883)
+    client.connect()
+    print("Connected to MQTT")
+    return client
+
+
 
 while True:
     dht_sensor.measure()
